42ed1e8aab4fb668f2d003b8ccc31cc3269fbf5c
Maxx Begole <maxxbegole@hotmail.com>
2019-08-23 07:41:38 -0500

    Link comment username to signed in user credentials.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-seeds();
+// seeds();
 
 // PASSPORT CONFIG
 app.use(require("express-session")({
